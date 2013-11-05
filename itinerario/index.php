69972e0eb301a315353165bdaef83d815ade3f90
Marcos <Marcos@PC-METAL>
2013-11-04 21:26:49 -0300

    Adicionando arquivos base

--- itinerario/index.php
+++ itinerario/index.php
@@ -1,17 +1,42 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="pt-br" xml:lang="pt-br">
     <head>
-        <meta charset="ISO-8859-1">
-        <title></title>
+        <title>Transporte Coletivo em Campo Grande</title>
+        
+        <!-- Metas -->
+		<meta name="Author" content="Marcos Rogério Germano - 8º Semestre de Ciência da Computação - FCG"/>
+		<meta name="Custodian" content="Marcos Rogério Germano - 8º Semestre de Ciência da Computação - FCG" />
+		<meta name="DC.Identifier" content=""/> 
+		<meta name="copyleft" content="© 2013 Marcos Rogério Germano - 8º Semestre de Ciência da Computação - FCG" />
+		<meta name="description" content="Exemplo prático para obtenção de diploma de Bacharelado" />
+		<meta name="keywords" content="mapa, transporte coletivo, rota" />
+		<meta http-equiv="Content-Language" content="pt-br"/>
+		<meta http-equiv="cache-control"   content="no-cache" />
+		<meta http-equiv="pragma" content="no-cache" />
+		<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        
+        <!-- Bootstrap -->
+        <link href="css/bootstrap.min.css" rel="stylesheet" media="screen" />
+        
+        <!-- CSS -->
+        <link rel="stylesheet"  type="text/css" href="css/estilo.css" />
+        
+
     </head>
+
     <body>
-        <?php
-        // put your code here
-        ?>
+        <div id="principal">
+            <h2>Hello World !!!</h2>
+            <div id="mapa"></div>
+        </div>
+
+
+
+
+        <!-- Scripts -->
+        <script type="text/javascript" src="js/jquery-min.js"></script>
+        <script type="text/javascript" src="js/padrao.js"></script>
+        <script src="js/bootstrap.min.js"></script>
     </body> 
 </html>
